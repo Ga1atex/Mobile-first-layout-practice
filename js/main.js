ea967b4e980b5c72cf560e592e65d11baa4e2c3b
Ga1atex <galatexxx@gmail.com>
2021-09-09 11:35:45 +0300

    completed first page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-$(function(){
+$('.top-slider').slick({
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: '<button type="button" class="top-slider__arrow top-slider__arrow-prev"> <img src="../images/arrow-prev.svg" alt="slider arrow-previous"></button>',
+  nextArrow: '<button type="button" class="top-slider__arrow top-slider__arrow-next"> <img src="../images/arrow-next.svg" alt="slider arrow-next"></button>',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
 
+      }
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        arrows: false,
+      }
+    },
+  ]
 });
