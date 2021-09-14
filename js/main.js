ab773e2e91a2ea1e291482f704e7f1440e684560
Ga1atex <galatexxx@gmail.com>
2021-09-14 17:13:28 +0300

    completed site

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ $('.top-slider').slick({
   dots: true,
   slidesToShow: 1,
   slidesToScroll: 1,
-  prevArrow: '<button type="button" class="top-slider__arrow top-slider__arrow-prev"> <img src="../images/arrow-prev.svg" alt="slider arrow-previous"></button>',
-  nextArrow: '<button type="button" class="top-slider__arrow top-slider__arrow-next"> <img src="../images/arrow-next.svg" alt="slider arrow-next"></button>',
+  prevArrow: '<button type="button" class="top-slider__arrow top-slider__arrow-prev button"> <img src="../images/arrow-prev.svg" alt="slider arrow-previous"></button>',
+  nextArrow: '<button type="button" class="top-slider__arrow top-slider__arrow-next button"> <img src="../images/arrow-next.svg" alt="slider arrow-next"></button>',
   responsive: [
     {
       breakpoint: 1024,
@@ -15,6 +15,28 @@ $('.top-slider').slick({
       breakpoint: 640,
       settings: {
         arrows: false,
+      }
+    },
+  ]
+});
+
+
+$('.quote-slider__items').slick({
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+
+      }
+    },
+    {
+      breakpoint: 640,
+      settings: {
+
       }
     },
   ]
