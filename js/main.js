5c5ee8b20ead9367b2b99ec54297881ed4579206
Ga1atex <galatexxx@gmail.com>
2021-09-15 13:31:25 +0300

    fixed top-slider arrows

--- js/main.js
+++ js/main.js
@@ -2,15 +2,9 @@ $('.top-slider').slick({
   dots: true,
   slidesToShow: 1,
   slidesToScroll: 1,
-  prevArrow: '<button type="button" class="top-slider__arrow top-slider__arrow-prev button"> <img src="../images/arrow-prev.svg" alt="slider arrow-previous"></button>',
-  nextArrow: '<button type="button" class="top-slider__arrow top-slider__arrow-next button"> <img src="../images/arrow-next.svg" alt="slider arrow-next"></button>',
+  prevArrow: '<button type="button" class="top-slider__arrow top-slider__arrow-prev button"> <img class="top-slider__arrow-prev-img" src="images/arrow-prev.svg" alt="slider arrow-previous"></button>',
+  nextArrow: '<button type="button" class="top-slider__arrow top-slider__arrow-next button"> <img class="top-slider__arrow-next-img" src="images/arrow-next.svg" alt="slider arrow-next"></button>',
   responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-
-      }
-    },
     {
       breakpoint: 640,
       settings: {
@@ -25,19 +19,5 @@ $('.quote-slider__items').slick({
   dots: true,
   slidesToShow: 1,
   slidesToScroll: 1,
-  arrows: false,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-
-      }
-    },
-    {
-      breakpoint: 640,
-      settings: {
-
-      }
-    },
-  ]
+  arrows: false
 });
